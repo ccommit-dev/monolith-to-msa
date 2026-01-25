38ad82e3f4fbf86d3956f1a884f465e38629011e
junshock5 <topojs@gmail.com>
2026-01-25 19:19:40 +0900

    git init

---
+++ src/test/java/com/ccommit/monolith_to_msa/MonolithToMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccommit.monolith_to_msa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithToMsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
